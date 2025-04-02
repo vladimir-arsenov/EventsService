f1125b6bd044a0cea066388e4416c1ed0b83908e
vladimir-arsenov <arsenovova7@gmail.com>
2025-04-02 21:22:49 +0300

    Implemented currency-service

--- currency-service/src/main/java/org/example/CurrencyServiceApplication.java
+++ currency-service/src/main/java/org/example/CurrencyServiceApplication.java
@@ -1,9 +1,11 @@
-package org.example.currencyservice;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CurrencyServiceApplication {
 
     public static void main(String[] args) {
